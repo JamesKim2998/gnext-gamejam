c787285398425e0c3f994d102b0862829f8dfc81
Unknown <com211938@ajou.ac.kr>
2017-07-22 03:41:22 +0900

    공 충돌 기능

---
+++ client/Assets/EnemyScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    public float power;
+    Rigidbody2D body;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        power = 4.0f;
+    }
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("collp");
+        GameObject target = coll.gameObject;
+
+        Vector3 inNormal = Vector3.Normalize(
+             target.transform.position - transform.position);
+        Vector3 bounceVector =
+            Vector3.Reflect(coll.relativeVelocity, inNormal);
+
+        Debug.Log("inNormaly : " + inNormal.y + " bounceVectory : " + bounceVector.y);
+
+        // 알아보기 쉽게 하기 위해 force값을 곱해 충돌시 속도를 20%로 줄였습니다. 
+        // 충돌했던 제 속도를 그대로 내시려면 수식에서 force 곱해주는 부분을 빼면 됩니다. 
+        target.GetComponent<Rigidbody2D>().AddForce(inNormal + (-inNormal * power), ForceMode2D.Impulse);
+    }
+}
---
+++ client/Assets/GameManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+    [Header("Player")]
+    public GameObject Player;
+    public float PlayerPower;
+
+    [Header("Player")]
+    public GameObject Enemy;
+    public float EnemyPower;
+
+    [Header("Ball")]
+    public GameObject Ball;
+
+    // Use this for initialization
+    void Start () {
+        PlayerPower = Player.GetComponent<PlayerScript>().power;
+        EnemyPower = Enemy.GetComponent<EnemyScript>().power;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
