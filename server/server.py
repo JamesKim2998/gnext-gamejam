228a584818bad7cf1459fc5305c9f75d3545bf02
김대원 <eodnjs2998@gmail.com>
2017-07-22 01:46:27 +0900

    server 코드 수정. (input을 받도록)

--- server/server.py
+++ server/server.py
@@ -3,7 +3,8 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-players = {}
+players_input = {}
+game_state = {}
 
 
 @app.route('/ping')
@@ -11,18 +12,28 @@ def ping():
     return 'OK'
 
 
-@app.route('/update_player', methods=['POST'])
-def update_player():
+@app.route('/get_players_input')
+def get_players_input():
+    return json.dumps(players_input)
+
+
+@app.route('/update_player_input', methods=['POST'])
+def update_players_input():
     device_id = request.args.get('device_id')
-    players[device_id] = request.json
-    players_except_me = {}
-    for test_device_id, test_player in players.items():
-        if device_id == test_device_id:
-            continue
-        players_except_me[test_device_id] = test_player
-    return json.dumps(players_except_me)
+    players_input[device_id] = request.json
+
+
+@app.route('/get_game_state')
+def get_game_state():
+    return json.dumps(game_state)
+
+
+@app.route('/update_game_state', methods=['POST'])
+def update_game_state():
+    global game_state
+    game_state = request.json
 
 
 @app.route('/inspect')
 def inspect():
-    return json.dumps(players)
+    return json.dumps(game_state)
