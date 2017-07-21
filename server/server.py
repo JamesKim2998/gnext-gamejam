7e571da8a4de1faa25c202b640550943a6398b3b
김대원 <eodnjs2998@gmail.com>
2017-07-22 00:59:38 +0900

    update_player 요청을 구현.

--- server/server.py
+++ server/server.py
@@ -1,7 +1,28 @@
-from flask import Flask
+import json
+from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/index')
-def hello_world():
-    return 'Hello, World!'
+players = {}
+
+
+@app.route('/ping')
+def ping():
+    return 'OK'
+
+
+@app.route('/update_player', methods=['POST'])
+def update_player():
+    device_id = request.args.get('device_id')
+    players[device_id] = request.json
+    players_except_me = {}
+    for test_device_id, test_player in players.items():
+        if device_id == test_device_id:
+            continue
+        players_except_me[test_device_id] = test_player
+    return json.dumps(players_except_me)
+
+
+@app.route('/inspect')
+def inspect():
+    return json.dumps(players)
