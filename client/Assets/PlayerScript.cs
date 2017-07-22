b9cf4881a70d574ec57a9fa34c6ca3513a84454f
Unknown <com211938@ajou.ac.kr>
2017-07-22 15:40:14 +0900

    임시버전

--- client/Assets/PlayerScript.cs
+++ client/Assets/PlayerScript.cs
@@ -5,14 +5,17 @@ using TouchControlsKit;
 using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour {
-    public float power;
+    public static float power;
     public GameObject HpBar;
     public Canvas UICanvas;
     GameObject PlayerHp;
     Rigidbody2D body;
-    public float speed;
-    public float SpinSpeed;
+    public static float speed;
+    public static float SpinSpeed;
     public TCKDPad Pad;
+
+    float PowerTime;
+    float SpeedTime;
 
 	// Use this for initialization
 	void Start () {
@@ -67,11 +70,66 @@ public class PlayerScript : MonoBehaviour {
             Debug.Log("ou");
             PlayerHp.GetComponent<Slider>().value -= 20;
         }
+
     }
 
-    /*private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.transform.tag == "")
-    }*/
+        if(collision.transform.tag == "item3")
+        {
+            collision.gameObject.SetActive(false);
+            StartCoroutine("PowerTimer");
+        }
+        if (collision.transform.tag == "item4")
+        {
+            collision.gameObject.SetActive(false);
+            StartCoroutine("SpeedTimer");
+        }
+        if (collision.transform.tag == "item5")
+        {
+            
+            Debug.Log("nooo");
+            GameObject target = collision.gameObject;
 
+            Vector3 inNormal = Vector3.Normalize(
+                 transform.position - target.transform.position);
+
+            this.GetComponent<Rigidbody2D>().AddForce(inNormal * 600 * speed, ForceMode2D.Impulse);
+            speed = 0.5f;
+        }
+        if(collision.transform.tag == "item6")
+        {
+            GameObject white = GameObject.FindWithTag("item7");
+            this.transform.position = white.transform.position;
+            collision.gameObject.SetActive(false);
+            white.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "item5")
+        {
+            collision.gameObject.SetActive(false);
+            speed = 6.0f;
+        }
+    }
+
+    IEnumerator PowerTimer()
+    {
+        Debug.Log("powerco");
+        power = 9.0f;
+        SpinSpeed = -30.0f;
+        yield return new WaitForSeconds(5.0f);
+        power = 4.0f;
+        SpinSpeed = -10.0f;
+    }
+
+    IEnumerator SpeedTimer()
+    {
+        Debug.Log("speedco");
+        speed = 10.0f;
+        yield return new WaitForSeconds(5.0f);
+        speed = 6.0f;
+    }
 }
