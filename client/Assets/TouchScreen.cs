8f64a94cb6bb99813c9540e6f3d3acac7d5cea64
Unknown <com211938@ajou.ac.kr>
2017-07-23 01:34:41 +0900

    터치 변경

--- client/Assets/TouchScreen.cs
+++ client/Assets/TouchScreen.cs
@@ -13,22 +13,29 @@ public class TouchScreen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetTouch(0).phase ==TouchPhase.Began)
-        //if (Input.GetMouseButtonDown(0))
+        if (Input.touchCount > 0)
         {
-            Debug.Log("hi");
-            Vector3 temppoint = Input.GetTouch(0).position;
-            StartPoint = Camera.main.ScreenToWorldPoint(temppoint);
-            Debug.Log(StartPoint);
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            //if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("hi");
+                Vector3 temppoint = Input.GetTouch(0).position;
+                StartPoint = temppoint;
+                Debug.Log(StartPoint);
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            //else if (Input.GetMouseButton(0))
+            {
+                Vector3 temppoint2 = Input.GetTouch(0).position;
+                EndPoint = temppoint2;
+                inNormal = Vector3.Normalize(EndPoint - StartPoint);
+                Debug.Log(inNormal);
+            }
+            else;
         }
-        else if(Input.GetTouch(0).phase == TouchPhase.Moved)
-        //else if (Input.GetMouseButton(0))
+        else
         {
-            Vector3 temppoint2 = Input.GetTouch(0).position;
-            EndPoint = Camera.main.ScreenToWorldPoint(temppoint2);
-            inNormal = Vector3.Normalize(EndPoint - StartPoint);
-            Debug.Log(inNormal);
+            inNormal = new Vector2(0.0f, 0.0f);
         }
-        else;
 	}
 }
