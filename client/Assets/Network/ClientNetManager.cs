ab01b87a8f497a7ad50b470721e5f33c728fa087
김대원 <eodnjs2998@gmail.com>
2017-07-23 03:43:12 +0900

    ClientNetManager 수정.

--- client/Assets/Network/ClientNetManager.cs
+++ client/Assets/Network/ClientNetManager.cs
@@ -6,6 +6,7 @@ public class ClientNetManager : MonoBehaviour
 
     private void Awake()
     {
+        _gameManager = GetComponent<GameManagerScript>();
         WSClientBroadcast.SmallNet += OnSmallNet;
         WSClientBroadcast.BigNet += OnBigNet;
     }
