23a423e592f5eebede0800afcb29a18ec3413220
김대원 <eodnjs2998@gmail.com>
2017-07-23 11:44:48 +0900

    FinishButton 복구.

--- client/Assets/FinishButton.cs
+++ client/Assets/FinishButton.cs
@@ -3,18 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class FinishButton : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
+public class FinishButton : MonoBehaviour
+{
     public void Finish()
     {
         SceneManager.LoadScene("main");
